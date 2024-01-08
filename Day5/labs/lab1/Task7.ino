67b4629618a1241e0db249e2ba32740aa0d17f03
Shahad <138361374+Shahadbal@users.noreply.github.com>
2024-01-08 23:13:16 +0300

    Add files via upload

---
+++ Day5/labs/lab1/Task7.ino
@@ -0,0 +1,59 @@
+#include "VL53L1X.h"
+VL53L1X proximity;
+
+int reading = 0;
+int maxBlinkTime = 2000; // Maximum time interval for LED blink
+int minBlinkTime = 200;  // Minimum time interval for LED blink
+int distanceThreshold = 300; // Set your desired threshold value
+
+void setup() {
+  Serial.begin(115200);
+  Wire1.begin();
+  Wire1.setClock(400000); // use 400 kHz I2C
+  proximity.setBus(&Wire1);
+
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+
+  if (!proximity.init()) {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1);
+  }
+
+  proximity.setDistanceMode(VL53L1X::Short);
+  proximity.setMeasurementTimingBudget(10000);
+  proximity.startContinuous(10);
+}
+
+void loop() {
+  reading = proximity.read();
+  Serial.println(reading);
+
+  // Send a message when the distance is shorter than the threshold
+  if (reading < distanceThreshold) {
+    Serial.println("Object is too close!");
+  }
+
+  // Map the reading to the RGB color scale (yellow to red)
+  int redValue = map(reading, 0, 1000, 255, 0);
+  int greenValue = map(reading, 0, 1000, 255, 255);
+  int blueValue = map(reading, 0, 1000, 0, 0);
+
+  analogWrite(LEDR, redValue);
+  analogWrite(LEDG, greenValue);
+  analogWrite(LEDB, blueValue);
+
+  // Adjust LED blink rate based on proximity reading
+  int blinkTime = map(reading, 0, 1000, maxBlinkTime, minBlinkTime);
+
+  // Implement LED blinking
+  static unsigned long timeStart = millis();
+  if (millis() - timeStart >= blinkTime) {
+    timeStart = millis();
+    // Toggle LED state
+    digitalWrite(LEDR, !digitalRead(LEDR));
+    digitalWrite(LEDG, !digitalRead(LEDG));
+    digitalWrite(LEDB, !digitalRead(LEDB));
+  }
+}
